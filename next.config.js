47f97d1155a915edad15f43c774a4e559f3651ed
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-03-03 13:48:18 +0200

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-  unstable_runtimeJS: false,
+const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
-
-  mergeDuplicateChunks: true,
-
-  experimental: {
-    swcFileReading: false,
-    legacyBrowsers: false,
-    browsersListForSwc: true,
-    images: { allowFutureImage: true },
-  },
-
-  compiler: {
-    styledComponents: true,
-  },
 
   images: {
     domains: ["image/png", "image/webp", "mdbootstrap.com"],
   },
 };
+
+module.exports = nextConfig;
